94a0e2c9c5511c0020826c51562db1d0fb368a4e
JitChowDhury <jitchowdhury8717@gmail.com>
2026-04-25 19:10:48 +0530

    add bg and walls

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,28 @@
 #include "raylib.h"
+#include <iostream>
+#include <filesystem>
 
 int main()
 {
+  SetConfigFlags(FLAG_WINDOW_RESIZABLE);
   InitWindow(1280, 720, "Swarm");
   SetTargetFPS(60);
+
+  Texture2D background = LoadTexture("Assets/images/Floor.png");
+  Texture2D walls = LoadTexture("Assets/images/Walls.png");
 
   while (!WindowShouldClose())
   {
     BeginDrawing();
     ClearBackground(BLACK);
-    DrawText("It works!", 24, 24, 20, WHITE);
+    DrawTexture(background, 0, 0, WHITE);
+    DrawTexture(walls, 0, 0, WHITE);
+    DrawText("", 24, 24, 20, WHITE);
+
     EndDrawing();
   }
 
+  UnloadTexture(background);
   CloseWindow();
   return 0;
 }
