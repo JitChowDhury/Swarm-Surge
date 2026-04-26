232361b4eddf280d212c5bb8166dd3b8c9e5e0a3
JitChowDhury <jitchowdhury8717@gmail.com>
2026-04-26 18:00:43 +0530

    Centering camera

--- src/main.cpp
+++ src/main.cpp
@@ -13,17 +13,24 @@ int main()
 
   Texture2D background = LoadTexture("Assets/images/Floor.png");
   Texture2D walls = LoadTexture("Assets/images/Walls.png");
-  Texture2D playerTex = LoadTexture("Assets/images/survivor-idle_shotgun_0.png");
+  // Texture2D playerTex = LoadTexture("Assets/images/survivor-idle_shotgun_0.png");
 
   RenderTexture2D canvas = LoadRenderTexture(BASE_W, BASE_H);
   SetTextureFilter(canvas.texture, TEXTURE_FILTER_BILINEAR);
 
+  Camera2D camera = {};
+  camera.zoom = 1.f;
+  camera.target = {400.f, 400.f};
+  camera.offset = {BASE_W * 0.5f, BASE_H * 0.5f};
+
   while (!WindowShouldClose())
   {
     BeginTextureMode(canvas);
+    BeginMode2D(camera);
     DrawTexture(background, 0, 0, WHITE);
-    DrawTextureEx(playerTex, {400.0f, 400.0f}, 0.0f, 0.3f, WHITE);
+    // DrawTextureEx(playerTex, {400.0f, 400.0f}, 0.0f, 0.3f, WHITE);
     DrawTexture(walls, 0, 0, WHITE);
+    EndMode2D();
     EndTextureMode();
 
     float scale = std::min(
@@ -45,7 +52,7 @@ int main()
 
   UnloadTexture(background);
   UnloadTexture(walls);
-  UnloadTexture(playerTex);
+  // UnloadTexture(playerTex);
   UnloadRenderTexture(canvas);
   CloseWindow();
   return 0;
