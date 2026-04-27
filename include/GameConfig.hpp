6c88de237829ad8b99e466e70a358a0fefb7f01e
JitChowDhury <jitchowdhury8717@gmail.com>
2026-04-27 22:59:47 +0530

    setup Components

---
+++ include/GameConfig.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace GameConfig
+{
+  const int BASE_W = 1280;
+  const int BASE_H = 720;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,26 +2,25 @@
 #include <iostream>
 #include <filesystem>
 #include <algorithm>
-const int BASE_W = 1280;
-const int BASE_H = 720;
+#include "GameConfig.hpp"
 
 int main()
 {
   SetConfigFlags(FLAG_WINDOW_RESIZABLE);
-  InitWindow(BASE_W, BASE_H, "Swarm");
+  InitWindow(GameConfig::BASE_W, GameConfig::BASE_H, "Swarm");
   SetTargetFPS(60);
 
   Texture2D background = LoadTexture("Assets/images/Floor.png");
   Texture2D walls = LoadTexture("Assets/images/Walls.png");
-  // Texture2D playerTex = LoadTexture("Assets/images/survivor-idle_shotgun_0.png");
+  Texture2D playerTex = LoadTexture("Assets/tds_textures/citizenplayer_handgun.png");
 
-  RenderTexture2D canvas = LoadRenderTexture(BASE_W, BASE_H);
+  RenderTexture2D canvas = LoadRenderTexture(GameConfig::BASE_W, GameConfig::BASE_H);
   SetTextureFilter(canvas.texture, TEXTURE_FILTER_BILINEAR);
 
   float mapW = (float)background.width;
   float mapH = (float)background.height;
-  float halfW = BASE_W * 0.5f;
-  float halfH = BASE_H * 0.5f;
+  float halfW = GameConfig::BASE_W * 0.5f;
+  float halfH = GameConfig::BASE_H * 0.5f;
 
   Camera2D camera = {};
   camera.zoom = 1.f;
@@ -46,25 +45,25 @@ int main()
     BeginMode2D(camera);
     ClearBackground(BLACK);
     DrawTexture(background, 0, 0, WHITE);
-    // DrawTextureEx(playerTex, {400.0f, 400.0f}, 0.0f, 0.3f, WHITE);
     DrawTexture(walls, 0, 0, WHITE);
+    DrawTextureEx(playerTex, {400.0f, 400.0f}, 0.0f, 1.0f, WHITE);
     EndMode2D();
 
-    DrawRectangle(0, BASE_H - 32, BASE_W, 32, ColorAlpha(DARKBLUE, 0.6f));
+    DrawRectangle(0, GameConfig::BASE_H - 32, GameConfig::BASE_W, 32, ColorAlpha(DARKBLUE, 0.6f));
     const char *labelText = TextFormat("Camera:%.0f,%.0f", camera.target.x, camera.target.y);
-    DrawText(labelText, 12, BASE_H - 24, 20, LIME);
+    DrawText(labelText, 12, GameConfig::BASE_H - 24, 20, LIME);
 
     EndTextureMode();
 
     float scale = std::min(
-        (float)GetScreenWidth() / BASE_W,
-        (float)GetScreenWidth() / BASE_H);
+        (float)GetScreenWidth() / GameConfig::BASE_W,
+        (float)GetScreenWidth() / GameConfig::BASE_H);
 
-    float offsetX = (GetScreenWidth() - BASE_W * scale) * 0.5f;
-    float offsetY = (GetScreenHeight() - BASE_H * scale) * 0.5f;
+    float offsetX = (GetScreenWidth() - GameConfig::BASE_W * scale) * 0.5f;
+    float offsetY = (GetScreenHeight() - GameConfig::BASE_H * scale) * 0.5f;
 
-    Rectangle src = {0, 0, (float)BASE_W, -(float)BASE_H};
-    Rectangle dest = {offsetX, offsetY, BASE_W * scale, BASE_H * scale};
+    Rectangle src = {0, 0, (float)GameConfig::BASE_W, -(float)GameConfig::BASE_H};
+    Rectangle dest = {offsetX, offsetY, GameConfig::BASE_W * scale, GameConfig::BASE_H * scale};
 
     BeginDrawing();
     ClearBackground(BLACK);
@@ -75,7 +74,7 @@ int main()
 
   UnloadTexture(background);
   UnloadTexture(walls);
-  // UnloadTexture(playerTex);
+  UnloadTexture(playerTex);
   UnloadRenderTexture(canvas);
   CloseWindow();
   return 0;
